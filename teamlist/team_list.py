c389ac77a5e16c89e27946f03f960be00ea4b79d
Aditya <adityakaushal0103@gmail.com>
2024-05-31 17:19:49 +0530

    Update team_list.py

--- teamlist/team_list.py
+++ teamlist/team_list.py
@@ -11,8 +11,7 @@ class StaffListCog(commands.Cog):
         self.staff_roles = self.load_staff_roles()
         self.update_interval = 600  # Update interval in seconds (e.g., 600 seconds = 10 minutes)
         self.channel_id = 1045701383430606879  # Replace CHANNEL_ID with the actual channel ID
-        self.webhook_url = "https://discord.com/api/webhooks/1246058850176467005/GLijVlr3Em4WG_TRNEjHHaaS0fKFgaukAJW0s4oigoaRS7SHBnu39R0yQreU6vWFqqST"  # Replace with your webhook URL
-        self.message_id = None  # Variable to store the message ID of the sent embed
+        self.staff_list_message = None  # Store the staff list message
         self.generate_staff_list_task = self.bot.loop.create_task(self.auto_update_staff_list())
 
     def load_staff_roles(self):
@@ -22,45 +21,23 @@ class StaffListCog(commands.Cog):
                 return data.get("roles", [])
         return []
 
-    def save_staff_roles(self):
-        with open(self.data_file, 'w') as file:
-            json.dump({"roles": self.staff_roles}, file, indent=4)
-
-    @commands.command()
-    async def add_role(self, ctx, role: discord.Role):
-        if role.id not in self.staff_roles:
-            self.staff_roles.append(role.id)
-            self.save_staff_roles()
-            await ctx.send(f"Role '{role.name}' added to the staff list.")
-            await self.update_staff_list()
-        else:
-            await ctx.send(f"Role '{role.name}' is already in the staff list.")
-
-    @commands.command()
-    async def remove_role(self, ctx, role: discord.Role):
-        if role.id in self.staff_roles:
-            self.staff_roles.remove(role.id)
-            self.save_staff_roles()
-            await ctx.send(f"Role '{role.name}' removed from the staff list.")
-            await self.update_staff_list()
-        else:
-            await ctx.send(f"Role '{role.name}' is not in the staff list.")
-
     async def generate_staff_list_embed(self):
         channel = self.bot.get_channel(self.channel_id)
         embed = discord.Embed(title="Our Staff", color=discord.Color.blue())
+        embed.set_thumbnail(url="https://images-ext-1.discordapp.net/external/ddLLWUutTzUJUABcpTJl_xFqke-4fNkje4IYxEiHiZM/%3Fformat%3Dwebp%26width%3D211%26height%3D196/https/images-ext-1.discordapp.net/external/R6VId5otSXLbdUlt4WtsUpmUlvBPeWCdGuafoWbsF_A/%253Fformat%253Dwebp%2526width%253D172%2526height%253D160/https/images-ext-1.discordapp.net/external/cm4p0Ewcngpp2El0CF6XTgaTwbXj0FKBEbxnox3uRlw/%25253Fsize%25253D240%252526quality%25253Dlossless/https/cdn.discordapp.com/emojis/1038110732383961209.webp?format=webp&width=160&height=148")  # Replace ICON_URL with the URL of the icon
         for role_id in self.staff_roles:
             role = channel.guild.get_role(role_id)
             if role:
                 members = role.members
                 member_status_list = [
-                    f"{member.display_name}: {self.get_status_emoji(member.status)} {member.status}"
+                    f"{self.get_status_emoji(member.status)} {member.display_name}"
                     for member in members
                 ]
                 if member_status_list:
                     embed.add_field(name=role.name, value="\n".join(member_status_list), inline=False)
                 else:
                     embed.add_field(name=role.name, value="No members", inline=False)
+        embed.set_image(url="https://images-ext-1.discordapp.net/external/Jix1PZm5CLa9S1B6_nnTwtDgZRR_P1ACE9-h2NeGtlA/%3Fformat%3Dwebp%26quality%3Dlossless%26width%3D1148%26height%3D280/https/images-ext-1.discordapp.net/external/fveQb3JWpUuhkPQ6lgZiNqQcssFLceGKIjNiL6xrd_0/%253Fformat%253Dwebp%2526quality%253Dlossless%2526width%253D1044%2526height%253D255/https/images-ext-2.discordapp.net/external/7oZZFuziueGVvFwbvwmUJnS1KnRASQGy00B7fK2UtU0/https/www.helloexmouth.co.uk/wp-content/uploads/spheader-meettheteam.png?format=webp&quality=lossless&width=870&height=212")  # Replace IMAGE_URL with the URL of the image
         return embed
 
     async def auto_update_staff_list(self):
@@ -69,13 +46,10 @@ class StaffListCog(commands.Cog):
         if channel:
             while not self.bot.is_closed():
                 embed = await self.generate_staff_list_embed()
-                if self.message_id:
-                    webhook = discord.Webhook.from_url(self.webhook_url, adapter=discord.RequestsWebhookAdapter())
-                    await webhook.edit_message(message_id=self.message_id, embed=embed)
+                if not self.staff_list_message:
+                    self.staff_list_message = await channel.send(embed=embed)
                 else:
-                    webhook = discord.Webhook.from_url(self.webhook_url, adapter=discord.RequestsWebhookAdapter())
-                    message = await webhook.send(embed=embed)
-                    self.message_id = message.id
+                    await self.staff_list_message.edit(embed=embed)
                 await asyncio.sleep(self.update_interval)
 
     def get_status_emoji(self, status):
